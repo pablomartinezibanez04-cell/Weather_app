ec9967f451b0c392ca5f572e5270c26b6eab7591
Pablo Martinez Ibañez <pablomartinezibanez04@gmail.com>
2025-12-26 18:28:27 +0100

    FIX: dockerfile fix

--- backend/src/router/endpoints.js
+++ backend/src/router/endpoints.js
@@ -18,9 +18,35 @@ router.get('/general-data', async (req, res) => {
         error: "lat y lon son obligatorios",
         });
     }
-    console.log('Fetching data from OpenWeather API');
+    console.log(`Fetching data from OpenWeather API with lat: ${lat} and lon: ${lon}`);
     const data = await fetch(`${baseURL}?lat=${lat}&lon=${lon}&units=${units}&lang=${lang}&appid=${appid}`);
     
+    res.status(200).json(await data.json());
+});
+
+router.get ('/data-by-date', async (req, res) => {
+    console.log('Received a request at /data-by-date');
+    const { lat, lon, date, units = "metric", lang = "en" } = req.query;
+
+    if (!lat || !lon || !date) {
+        console.log('Missing required query parameters');
+        return res.status(400).json({
+        error: "lat, lon y date son obligatorios",
+        });  
+    }
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+        console.log('Invalid date format');
+        return res.status(400).json({
+            error: "El parámetro date debe estar en formato YYYY-MM-DD",
+        });
+    }
+    console.log(`Comverting date ${date} to UNIX UTC timestamp`);
+    const unixUtc = Math.floor(new Date(`${date}T00:00:00Z`).getTime() / 1000);
+    console.log(`Converted date to UNIX UTC timestamp: ${unixUtc}`);
+
+    console.log(`Fetching data from OpenWeather API for specific date: ${date}, lat: ${lat} and lon: ${lon}`);
+    const data = await fetch(`${baseURL}/timemachine?lat=${lat}&lon=${lon}&dt=${unixUtc}&units=${units}&lang=${lang}&appid=${appid}`);
+
     res.status(200).json(await data.json());
 });
 
